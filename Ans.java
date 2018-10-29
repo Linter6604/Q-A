c682b648b400182d5ce95b1908b546c33d4c3da5
Linter6604 <44563096+Linter6604@users.noreply.github.com>
2018-10-29 13:41:41 +0800

    Add files via upload

---
+++ Ans.java
@@ -0,0 +1,12 @@
+
+package JustTesting;
+
+public class Ans {
+    String prompt;
+    String answer;
+
+    public Ans(String prompt,String answer){
+        this.prompt=prompt;
+        this.answer=answer;
+    }
+}
